f2e18f49845c86b8d20923826ef4a3dd8590eeef
Beshencev <beshencev_serega@mail.ru>
2022-09-01 11:48:11 +0300

    первый комит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+
+    // задание 1
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    private static void task1() {
+        int clientOs = 1;
+
+        if (clientOs == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
+
+
