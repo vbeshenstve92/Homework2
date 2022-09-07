6d74c1c99fb5be15793bfde9a64f52112d1eeb2a
Beshencev <beshencev_serega@mail.ru>
2022-09-08 00:46:57 +0300

    дз

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,82 @@ public class Main {
     }
 
     private static void task1() {
-        int clientOs = 1;
+        int clientOS = 1;
 
-        if (clientOs == 0) {
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
-}
 
+    public static class Main2 {
+
+        // задание 2
+        public static void main(String[] args) {
+            task2();
+        }
+
+        public static void task2() {
+            int clientOS = 0;
+            int clientDeviceYear = 2020;
+
+            if (clientOS == 0) {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+            } else {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+            }
+        }
+    }
+
+
+    public static class Main3 {
+
+        // задание 3
+        public static void main(String[] args) {
+            task3();
+        }
+
+        public static void task3() {
+            int year = 2020;
+
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                System.out.println(year + "являеться высокостным");
+            } else {
+                System.out.println(year + "являеться не высокостным");
+            }
+        }
+    }
+
+    public static class Main4 {
+
+        // задание 4
+        public static void main(String[] args) {
+            task4();
+        }
+
+
+        public static void task4() {
+            int diliveryDistance = 95;
+
+            int deliveryDays = 1;
+
+                    if (diliveryDistance > 20) {
+                        deliveryDays++;
+                    }
+                    if (diliveryDistance > 60) {
+                        deliveryDays++;
+                    }
+                    System.out.println("Потребуется дней: " + deliveryDays);
+        }
+    }
+}
 
---
+++ src/Main5.java
@@ -0,0 +1,33 @@
+public class Main5 {
+    // задание 5
+    public static void main(String[] args) { task5(); }
+
+    public static void task5() {
+        int monthNumber = 12;
+
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Некоректный месяц: " + monthNumber);
+        }
+    }
+}
